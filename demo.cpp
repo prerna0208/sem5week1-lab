fd6a12afaff612ed3411c16a757d21371ac1e988
prerna0208 <124525603+prerna0208@users.noreply.github.com>
2023-08-06 20:41:29 +0530

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"This is a demo file uploaded on github as a part of submission on Lab Assignment for the Open Source Lab."<<endl;
+    cout<<"Assignment for Week-1.";
+    return 0;
+}
